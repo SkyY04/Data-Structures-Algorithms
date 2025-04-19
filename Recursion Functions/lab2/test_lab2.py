cd076d878a5af1a0e7ed48f1b67e76d4f23f80ea
Sky Yoon <133569965+SkyY04@users.noreply.github.com>
2025-04-19 15:23:07 +0000

    tested

--- Recursion Functions/lab2/test_lab2.py
+++ Recursion Functions/lab2/test_lab2.py
@@ -1,45 +1,35 @@
-#
-#   Author: Catherine Leung
-#   These are the unit tests for functions and classes of lab 2
-#   To use this, run: python test_lab2.py
-#   Tested using Python 3
-#
-
 import unittest
-from lab2 import factorial,linear_search,binary_search
+from lab2 import factorial, linear_search, binary_search
 
-class Lab1TestCase(unittest.TestCase):
-    """These are the test cases for functions and classes of lab2"""
-    
-    def test_linear_search(self):
-        mylist = [34, 1, 18, 20, 25, 30, 15, 16, 17, 22, 24, 31, 163, 9, 33, 55]
-        self.assertEqual(linear_search(mylist,0), -1)
-        self.assertEqual(linear_search(mylist,19), -1)
-        self.assertEqual(linear_search(mylist,164), -1)
-        curr = 0
-        for i in mylist:
-            self.assertEqual(linear_search(mylist,i),curr)
-            curr+=1
+class Lab2TestCase(unittest.TestCase):
+    """Custom test cases for lab2 functions"""
 
-    def test_factorial(self):
-        self.assertEqual(factorial(0),1)
-        self.assertEqual(factorial(1),1)
-        self.assertEqual(factorial(19),121645100408832000)
-        self.assertEqual(factorial(8),40320)
+    def test_linear_search_custom(self):
+        sample = [99, 88, 77, 66, 55, 44, 33, 22, 11, 0]
+        # Searching for values not in the list
+        self.assertEqual(linear_search(sample, 100), -1)
+        self.assertEqual(linear_search(sample, -1), -1)
+        # Searching for existing values
+        for idx, val in enumerate(sample):
+            self.assertEqual(linear_search(sample, val), idx)
 
+    def test_factorial_custom(self):
+        # Small inputs
+        self.assertEqual(factorial(2), 2)
+        self.assertEqual(factorial(3), 6)
+        self.assertEqual(factorial(4), 24)
+        # Larger input
+        self.assertEqual(factorial(10), 3628800)
 
-    def test_binary_search(self):
-        mylist = [1, 2, 4, 5, 8, 10, 15, 22, 27, 29, 30, 33, 55, 81, 100, 108, 200, 205, 310, 315]
-        self.assertEqual(binary_search(mylist,0),-1)
-        self.assertEqual(binary_search(mylist,19),-1)
-        self.assertEqual(binary_search(mylist,201),-1)
-        self.assertEqual(binary_search(mylist,320),-1)
-        curr = 0
-        for i in mylist:
-            self.assertEqual(binary_search(mylist,i),curr)
-            curr+=1
+    def test_binary_search_custom(self):
+        sorted_list = list(range(0, 101, 5))  # [0, 5, 10, ..., 100]
+        # Values that should not be found
+        self.assertEqual(binary_search(sorted_list, -10), -1)
+        self.assertEqual(binary_search(sorted_list, 3), -1)
+        self.assertEqual(binary_search(sorted_list, 102), -1)
+        # Values that should be found
+        for index, value in enumerate(sorted_list):
+            self.assertEqual(binary_search(sorted_list, value), index)
 
-
- 
 if __name__ == '__main__':
     unittest.main()
