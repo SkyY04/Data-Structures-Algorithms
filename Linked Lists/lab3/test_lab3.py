ef864c133bfaf96947e762c4c1372c963d9d95d3
Sky Yoon <133569965+SkyY04@users.noreply.github.com>
2025-04-21 20:54:52 +0000

    tested lab3

--- Linked Lists/lab3/test_lab3.py
+++ Linked Lists/lab3/test_lab3.py
@@ -1,332 +1,93 @@
-#
-#   Author: Catherine Leung
-#   These are the unit tests for functions and classes of assingment1
-#   To use this, run: python test_lab3.py
-
 import unittest
 from lab3 import DoublyLinked, Sentinel
 
-class Lab3TestCase(unittest.TestCase):
-	"""These are the test cases for functions and classes of a1"""
-	
-	def test_init_(self):
-		my_list = DoublyLinked()
-		self.assertEqual(my_list.get_front(), None)
-		self.assertEqual(my_list.get_back(), None)
- 
-	def test_push_front(self):
-		my_list = DoublyLinked()
-		my_list.push_front(1)
-		self.assertNotEqual(my_list.get_front(), None)
-		self.assertNotEqual(my_list.get_back(), None)
-		self.assertEqual(my_list.get_front().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_data(), 1)
-
-		my_list.push_front(5)
-		self.assertEqual(my_list.get_front().get_data(), 5)
-		self.assertEqual(my_list.get_back().get_data(), 1)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 5)
-
-
-		my_list.push_front(3)
-		self.assertEqual(my_list.get_front().get_data(), 3)
-		self.assertEqual(my_list.get_back().get_data(), 1)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 5)
-		self.assertEqual(my_list.get_front().get_next().get_next().get_data(), 1)
-		self.assertEqual(my_list.get_front().get_next().get_previous().get_data(), 3)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 5)
-
-		my_list.push_front(15)
-		self.assertEqual(my_list.get_front().get_data(), 15)
-		self.assertEqual(my_list.get_back().get_data(), 1)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 3)
-		self.assertEqual(my_list.get_front().get_next().get_next().get_data(), 5)
-		self.assertEqual(my_list.get_front().get_next().get_previous().get_data(), 15)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 5)
-
-
- 
-	def test_push_back(self):
-		my_list = DoublyLinked()
-		my_list.push_back(1)
-		self.assertEqual(my_list.get_front().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_data(), 1)
-
-		my_list.push_back(5)
-		self.assertEqual(my_list.get_front().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_data(), 5)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 5)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 1)
-
-
-		my_list.push_back(3)
-		self.assertEqual(my_list.get_front().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_data(), 3)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 5)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 5)
-		self.assertEqual(my_list.get_back().get_previous().get_previous().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_previous().get_next().get_data(), 3)
-
-		my_list.push_back(15)
-		self.assertEqual(my_list.get_front().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_data(), 15)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 5)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 3)
-		self.assertEqual(my_list.get_back().get_previous().get_previous().get_data(), 5)
-		self.assertEqual(my_list.get_back().get_previous().get_next().get_data(), 15)
-
-
-	def test_pop_front(self):
-		my_list = DoublyLinked()
-		my_list.push_front(15)
-		my_list.push_back(24)
-		my_list.push_front(31)
-		my_list.push_back(9)
-
-
-		rc = my_list.pop_front()
-		self.assertEqual(rc, 31)
-		self.assertEqual(my_list.get_front().get_data(), 15)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 24)
-		self.assertEqual(my_list.get_front().get_next().get_next().get_data(), 9)
-		self.assertEqual(my_list.get_back().get_data(), 9)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 24)
-		self.assertEqual(my_list.get_back().get_previous().get_previous().get_data(), 15)
-
-
-		rc = my_list.pop_front()
-		self.assertEqual(rc, 15)
-		self.assertEqual(my_list.get_front().get_data(), 24)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 9)
-		self.assertEqual(my_list.get_back().get_data(), 9)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 24)
-
-
-		rc = my_list.pop_front()
-		self.assertEqual(rc, 24)
-		self.assertEqual(my_list.get_front().get_data(), 9)
-		self.assertEqual(my_list.get_back().get_data(), 9)
-
-
-		rc = my_list.pop_front()
-		self.assertEqual(rc, 9)
-		self.assertEqual(my_list.get_front(), None)
-		self.assertEqual(my_list.get_back(), None)
-
-		with self.assertRaises(IndexError) as cm:
-			rc = my_list.pop_front()
-		self.assertEqual(str(cm.exception), 'pop_front() used on empty list')
-
- 
-	def test_pop_back(self):
-		my_list = DoublyLinked()
-		my_list.push_front(15)
-		my_list.push_back(24)
-		my_list.push_front(31)
-		my_list.push_back(9)
-
-
-
-		rc = my_list.pop_back()
-		self.assertEqual(rc, 9)
-		self.assertEqual(my_list.get_front().get_data(), 31)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 15)
-		self.assertEqual(my_list.get_front().get_next().get_next().get_data(), 24)
-		self.assertEqual(my_list.get_back().get_data(), 24)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 15)
-		self.assertEqual(my_list.get_back().get_previous().get_previous().get_data(), 31)
-
-
-		rc = my_list.pop_back()
-		self.assertEqual(rc, 24)
-		self.assertEqual(my_list.get_front().get_data(), 31)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 15)
-		self.assertEqual(my_list.get_back().get_data(), 15)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 31)
-
-
-		rc = my_list.pop_back()
-		self.assertEqual(rc, 15)
-		self.assertEqual(my_list.get_front().get_data(), 31)
-		self.assertEqual(my_list.get_back().get_data(), 31)
-
-
-		rc = my_list.pop_back()
-		self.assertEqual(rc, 31)
-		self.assertEqual(my_list.get_front(), None)
-		self.assertEqual(my_list.get_back(), None)
-
-		with self.assertRaises(IndexError) as cm:
-			rc = my_list.pop_back()
-		self.assertEqual(str(cm.exception), 'pop_back() used on empty list')
-
-
-
-
-
-
-
-	def test_sentinel_init_(self):
-		my_list = Sentinel()
-		self.assertEqual(my_list.get_front(), None)
-		self.assertEqual(my_list.get_back(), None)
- 
-	def test_sentinel_push_front(self):
-		my_list = Sentinel()
-		my_list.push_front(1)
-		self.assertNotEqual(my_list.get_front(), None)
-		self.assertNotEqual(my_list.get_back(), None)
-		self.assertEqual(my_list.get_front().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_data(), 1)
-
-		my_list.push_front(5)
-		self.assertEqual(my_list.get_front().get_data(), 5)
-		self.assertEqual(my_list.get_back().get_data(), 1)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 5)
-
-
-		my_list.push_front(3)
-		self.assertEqual(my_list.get_front().get_data(), 3)
-		self.assertEqual(my_list.get_back().get_data(), 1)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 5)
-		self.assertEqual(my_list.get_front().get_next().get_next().get_data(), 1)
-		self.assertEqual(my_list.get_front().get_next().get_previous().get_data(), 3)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 5)
-
-		my_list.push_front(15)
-		self.assertEqual(my_list.get_front().get_data(), 15)
-		self.assertEqual(my_list.get_back().get_data(), 1)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 3)
-		self.assertEqual(my_list.get_front().get_next().get_next().get_data(), 5)
-		self.assertEqual(my_list.get_front().get_next().get_previous().get_data(), 15)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 5)
-
-
- 
-	def test_sentinel_push_back(self):
-		my_list = Sentinel()
-		my_list.push_back(1)
-		self.assertEqual(my_list.get_front().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_data(), 1)
-
-		my_list.push_back(5)
-		self.assertEqual(my_list.get_front().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_data(), 5)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 5)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 1)
-
-
-		my_list.push_back(3)
-		self.assertEqual(my_list.get_front().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_data(), 3)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 5)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 5)
-		self.assertEqual(my_list.get_back().get_previous().get_previous().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_previous().get_next().get_data(), 3)
-
-		my_list.push_back(15)
-		self.assertEqual(my_list.get_front().get_data(), 1)
-		self.assertEqual(my_list.get_back().get_data(), 15)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 5)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 3)
-		self.assertEqual(my_list.get_back().get_previous().get_previous().get_data(), 5)
-		self.assertEqual(my_list.get_back().get_previous().get_next().get_data(), 15)
-
-
-	def test_sentinel_pop_front(self):
-		my_list = Sentinel()
-		my_list.push_front(15)
-		my_list.push_back(24)
-		my_list.push_front(31)
-		my_list.push_back(9)
-
-
-		rc = my_list.pop_front()
-		self.assertEqual(rc, 31)
-		self.assertEqual(my_list.get_front().get_data(), 15)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 24)
-		self.assertEqual(my_list.get_front().get_next().get_next().get_data(), 9)
-		self.assertEqual(my_list.get_back().get_data(), 9)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 24)
-		self.assertEqual(my_list.get_back().get_previous().get_previous().get_data(), 15)
-
-
-		rc = my_list.pop_front()
-		self.assertEqual(rc, 15)
-		self.assertEqual(my_list.get_front().get_data(), 24)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 9)
-		self.assertEqual(my_list.get_back().get_data(), 9)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 24)
-
-
-		rc = my_list.pop_front()
-		self.assertEqual(rc, 24)
-		self.assertEqual(my_list.get_front().get_data(), 9)
-		self.assertEqual(my_list.get_back().get_data(), 9)
-
-
-		rc = my_list.pop_front()
-		self.assertEqual(rc, 9)
-		self.assertEqual(my_list.get_front(), None)
-		self.assertEqual(my_list.get_back(), None)
-
-		with self.assertRaises(IndexError) as cm:
-			rc = my_list.pop_front()
-		self.assertEqual(str(cm.exception), 'pop_front() used on empty list')
-
- 
-	def test_sentinel_pop_back(self):
-		my_list = Sentinel()
-		my_list.push_front(15)
-		my_list.push_back(24)
-		my_list.push_front(31)
-		my_list.push_back(9)
-
-
-
-		rc = my_list.pop_back()
-		self.assertEqual(rc, 9)
-		self.assertEqual(my_list.get_front().get_data(), 31)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 15)
-		self.assertEqual(my_list.get_front().get_next().get_next().get_data(), 24)
-		self.assertEqual(my_list.get_back().get_data(), 24)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 15)
-		self.assertEqual(my_list.get_back().get_previous().get_previous().get_data(), 31)
-
-
-		rc = my_list.pop_back()
-		self.assertEqual(rc, 24)
-		self.assertEqual(my_list.get_front().get_data(), 31)
-		self.assertEqual(my_list.get_front().get_next().get_data(), 15)
-		self.assertEqual(my_list.get_back().get_data(), 15)
-		self.assertEqual(my_list.get_back().get_previous().get_data(), 31)
-
-
-		rc = my_list.pop_back()
-		self.assertEqual(rc, 15)
-		self.assertEqual(my_list.get_front().get_data(), 31)
-		self.assertEqual(my_list.get_back().get_data(), 31)
-
-
-		rc = my_list.pop_back()
-		self.assertEqual(rc, 31)
-		self.assertEqual(my_list.get_front(), None)
-		self.assertEqual(my_list.get_back(), None)
-
-		with self.assertRaises(IndexError) as cm:
-			rc = my_list.pop_back()
-		self.assertEqual(str(cm.exception), 'pop_back() used on empty list')
-
-
-
-
-
-
-
-
-
+class TestDoublyLinked(unittest.TestCase):
+
+    def test_push_front(self):
+        dll = DoublyLinked()
+        dll.push_front(10)
+        self.assertEqual(dll.get_front().get_data(), 10)
+        dll.push_front(20)
+        self.assertEqual(dll.get_front().get_data(), 20)
+        self.assertEqual(dll.get_back().get_data(), 10)
+
+    def test_push_back(self):
+        dll = DoublyLinked()
+        dll.push_back(30)
+        self.assertEqual(dll.get_back().get_data(), 30)
+        dll.push_back(40)
+        self.assertEqual(dll.get_back().get_data(), 40)
+        self.assertEqual(dll.get_front().get_data(), 30)
+
+    def test_pop_front(self):
+        dll = DoublyLinked()
+        dll.push_back(50)
+        dll.push_back(60)
+        val = dll.pop_front()
+        self.assertEqual(val, 50)
+        self.assertEqual(dll.get_front().get_data(), 60)
+
+    def test_pop_back(self):
+        dll = DoublyLinked()
+        dll.push_front(70)
+        dll.push_front(80)
+        val = dll.pop_back()
+        self.assertEqual(val, 70)
+        self.assertEqual(dll.get_back().get_data(), 80)
+
+    def test_pop_front_empty(self):
+        dll = DoublyLinked()
+        with self.assertRaises(IndexError):
+            dll.pop_front()
+
+    def test_pop_back_empty(self):
+        dll = DoublyLinked()
+        with self.assertRaises(IndexError):
+            dll.pop_back()
+
+class TestSentinel(unittest.TestCase):
+
+    def test_push_front(self):
+        sll = Sentinel()
+        sll.push_front(100)
+        self.assertEqual(sll.get_front().get_data(), 100)
+        sll.push_front(110)
+        self.assertEqual(sll.get_front().get_data(), 110)
+        self.assertEqual(sll.get_back().get_data(), 100)
+
+    def test_push_back(self):
+        sll = Sentinel()
+        sll.push_back(200)
+        self.assertEqual(sll.get_back().get_data(), 200)
+        sll.push_back(210)
+        self.assertEqual(sll.get_back().get_data(), 210)
+        self.assertEqual(sll.get_front().get_data(), 200)
+
+    def test_pop_front(self):
+        sll = Sentinel()
+        sll.push_back(300)
+        sll.push_back(310)
+        val = sll.pop_front()
+        self.assertEqual(val, 300)
+        self.assertEqual(sll.get_front().get_data(), 310)
+
+    def test_pop_back(self):
+        sll = Sentinel()
+        sll.push_front(400)
+        sll.push_front(410)
+        val = sll.pop_back()
+        self.assertEqual(val, 400)
+        self.assertEqual(sll.get_back().get_data(), 410)
+
+    def test_pop_front_empty(self):
+        sll = Sentinel()
+        with self.assertRaises(IndexError):
+            sll.pop_front()
+
+    def test_pop_back_empty(self):
+        sll = Sentinel()
+        with self.assertRaises(IndexError):
+            sll.pop_back()
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
